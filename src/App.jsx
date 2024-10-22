a976fb7bc5e2ad9e3e56c6112b8bbf43223bf50b
Fanny Dacheux <fanny.dacheux@gmail.com>
2024-10-22 11:34:50 +0200

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import UpdateTimeButton from "./components/UpdateTimeButton";
+import ToggleButton from "./components/ToggleButton";
+import { useSelector } from "react-redux";
+import getFormattedValue from "./utils/getFormattedValue";
+
+function App() {
+  const chronoValues = useSelector((state) => state.chrono);
+  return (
+    <div className="bg-slate-700 text-slate-100 pt-20 min-h-screen">
+      <div className="max-w-xl mx-auto border border-slate-500 rounded p-10">
+        <h1 className="text-center text-3xl mb-8">Pomodoro App</h1>
+        <div className="flex justify-center mb-8">
+          {/*Session block */}
+          <div className="mr-10">
+            <p className="text-center mb-1">Sessions</p>
+            <div className="flex">
+              <UpdateTimeButton sign={"-"} type={"session"} />
+              <p className="mx-4 text-xl">{chronoValues.session.value / 60}</p>
+              <UpdateTimeButton sign={"+"} type={"session"} />
+            </div>
+          </div>
+          {/* Pause block */}
+          <div>
+            <p className="text-center mb-1">Pauses</p>
+            <div className="flex">
+              <UpdateTimeButton sign={"-"} type={"pause"} />
+              <p className="mx-4 text-xl">{chronoValues.pause.value / 60}</p>
+              <UpdateTimeButton sign={"+"} type={"pause"} />
+            </div>
+          </div>
+        </div>
+        <p className="text-center mb-2 text-xl font-semibold">
+          {chronoValues.displayedValue.heading}
+        </p>
+        <p className="text-center flex justify-center mb-1">
+          <span className="text-4xl p-4 rounded bg-slate-300 text-slate-900">
+            {getFormattedValue(chronoValues.displayedValue.value)}
+          </span>
+        </p>
+        <p className="mb-10 text-center">
+          Passed cycle(s) : {chronoValues.cycles}
+        </p>
+        <ToggleButton />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/chrono.js
@@ -0,0 +1,104 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  session: {
+    value: 1500,
+    runningValue: 1500,
+  },
+  pause: {
+    value: 300,
+    runningValue: 300,
+  },
+  isPlaying: false,
+  intervalID: undefined,
+  cycles: 0,
+  displayedValue: {
+    value: 1500,
+    heading: "Work",
+  },
+};
+
+export const chrono = createSlice({
+  name: "chrono",
+  initialState,
+  reducers: {
+    updateChronoValues: (state, action) => {
+      const chosenState = state[action.payload.type];
+
+      if (chosenState.value + action.payload.value === 0) return;
+
+      if (action.payload.type === "session") {
+        if (state.session.value === state.session.runningValue) {
+          chosenState.value = chosenState.value + action.payload.value;
+          chosenState.runningValue =
+            chosenState.runningValue + action.payload.value;
+          state.displayedValue.value = chosenState.runningValue;
+        } else {
+          chosenState.value = chosenState.value + action.payload.value;
+        }
+      }
+
+      if (action.payload.type === "pause") {
+        chosenState.value = chosenState.value + action.payload.value;
+        // chosenState.runningValue =
+        //   chosenState.runningValue + action.payload.value;
+        chosenState.runningValue =
+          !state.isPlaying && chosenState.value != chosenState.runningValue
+            ? chosenState.value
+            : chosenState.runningValue;
+      }
+    },
+    tick: (state, action) => {
+      if (state.session.runningValue > 0) {
+        state.session.runningValue--;
+        state.displayedValue.value = state.session.runningValue;
+        state.displayedValue.heading = "Work";
+      } else if (state.pause.runningValue > 0) {
+        state.pause.runningValue--;
+        console.log(state.pause.runningValue);
+        state.displayedValue.value = state.pause.runningValue;
+        state.displayedValue.heading = "Pause";
+      } else {
+        state.cycles++;
+        state.session.runningValue = state.session.value - 1;
+        state.displayedValue.value = state.session.value - 1;
+        state.displayedValue.heading = "Work";
+        state.pause.runningValue = state.pause.value;
+      }
+    },
+    setUpChrono: (state, action) => {
+      state.isPlaying = true;
+      state.intervalID = action.payload;
+    },
+    stopChrono: (state, action) => {
+      window.clearInterval(state.intervalID);
+      state.isPlaying = false;
+    },
+    resetChrono: (state, action) => {
+      window.clearInterval(state.intervalID);
+      state.isPlaying = false;
+      state.session.runningValue = state.session.value;
+      state.pause.runningValue = state.pause.value;
+      state.displayedValue.value = state.session.runningValue;
+      state.cycles = 0;
+    },
+  },
+});
+
+export function startChrono(action) {
+  return function (dispatch, getState) {
+    const intervalID = setInterval(() => {
+      dispatch(tick());
+    }, 1000);
+    dispatch(setUpChrono(intervalID));
+    dispatch(tick());
+  };
+}
+export const {
+  updateChronoValues,
+  stopChrono,
+  setUpChrono,
+  tick,
+  resetChrono,
+} = chrono.actions;
+export default chrono.reducer;
